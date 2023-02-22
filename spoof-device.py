4efd8e5812721bddef342a5210e537660e33093b
Irving A. Bermudez <bigchirv@gmail.com>
2023-02-21 20:32:27 -0500

    Trying a cleaner version of the code

--- spoof-device.py
+++ spoof-device.py
@@ -31,47 +31,35 @@ if "-d" in sys.argv:
     
 else:
     print("\nUsage:\n")
-    print("       ", sys.argv[0], "-d <device> [--quirk-fix]\n")
+    print("       ", sys.argv[0], "-d <device>\n")
     print("       ", sys.argv[0], "--list-devices\n")
     sys.exit(0)
 
 print("Using: " + device.path + " (" + device.name + ")")
-idVendorProduct = "" + str(device.info.vendor) + ":" + str(device.info.product)
 
 caps = {
-        ecodes.EV_MSC : [ecodes.MSC_SCAN],
-        }
+    ecodes.EV_MSC: [ecodes.MSC_SCAN],
+    ecodes.EV_ABS: [ecodes.ABS_X, ecodes.ABS_Y],
+    ecodes.EV_KEY: [ecodes.BTN_JOYSTICK, ecodes.BTN_TRIGGER]
+}
 
 deviceCapabilities = device.capabilities()
 if ecodes.EV_ABS in deviceCapabilities:
-    caps[ecodes.EV_ABS] = deviceCapabilities[ecodes.EV_ABS]
-else:
-    # Some devices do not have axis, so fake at least two axis
-    caps[ecodes.EV_ABS] = [ecodes.ABS_X, ecodes.ABS_Y]
+    caps[ecodes.EV_ABS] += deviceCapabilities[ecodes.EV_ABS]
 
 if ecodes.EV_KEY in deviceCapabilities:
     caps[ecodes.EV_KEY] = deviceCapabilities[ecodes.EV_KEY]
-else:
-    # Some devices do not have buttons, so fake at least one button
-    caps[ecodes.EV_KEY] = [ecodes.BTN_JOYSTICK, ecodes.BTN_TRIGGER]
 
-# Quirk Fix: For pedals that may need the extra X/Y axis,
-# e.g. ATMEL/VIRPIL/191105 VPC Rudder Pedals, VKBsim T-Rudder 
-if "--quirk-fix" in sys.argv:
-    print ("Warning: Applying quirk (add X/Y axis) for " + device.name +  "!")
-    caps[ecodes.EV_ABS].append(ecodes.ABS_X)
-    caps[ecodes.EV_ABS].append(ecodes.ABS_Y)
-
-spoofdevice = evdev.uinput.UInput(events=caps,
+spoofDevice = evdev.uinput.UInput(events=caps,
                                   name=device.name + " PRO",
                                   vendor=device.info.vendor, 
                                   product=device.info.product,
                                   version=device.info.version)
 
-print("Spoofing:", spoofdevice)
-print("Mirroring events. Press Ctrl-C twice for exit.")
+print("Spoofing:", spoofDevice)
+print("Mirroring events. Press Ctrl-C twice to exit.")
 
 for event in device.read_loop():
-    spoofdevice.write_event(event)
-    spoofdevice.syn()
+    spoofDevice.write_event(event)
+    spoofDevice.syn()
 
